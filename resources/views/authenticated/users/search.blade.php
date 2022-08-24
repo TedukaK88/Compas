7457e5bfe6034f0c2830150d30e32b56303535e6
Surumeika-PC\Surumeika <tedukakazuma88@gmail.com>
2022-08-24 13:16:19 +0900

    subject_usersリレーション定義、及びユーザー検索機能の実装完了

--- resources/views/authenticated/users/search.blade.php
+++ resources/views/authenticated/users/search.blade.php
@@ -43,7 +43,9 @@
       </div>
       <div>
         @if($user->role == 4)
-        <span>選択科目 :</span>
+          @foreach($user->subjects as $user_subjects)
+            <span>選択科目 :{{ $user_subjects->subject }}</span>
+          @endforeach
         @endif
       </div>
     </div>
@@ -88,6 +90,12 @@
           </div>
           <div class="selected_engineer">
             <label>選択科目</label>
+            @foreach($subjects as $subject)
+            <div class="">
+              <input type="checkbox" name="subject" form="userSearchRequest" value="{{ $subject->id }}">
+              <label>{{ $subject->subject }}</label>
+            </div>
+            @endforeach
           </div>
         </div>
       </div>
